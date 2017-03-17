8d23b6928a978b51993d23880253d26724d14b11
Michael Petrosyan <mikpetr95@gmail.com>
2017-03-17 10:52:50 +0400

    temp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to my React chat</h2>
+          </div>
+          <p className="App-intro">
+            This is a simple chat, that you can use for everyday :).
+          </p>
+          <p>
+            To get started, type your name.
+          </p>
+          <p className="inputs-group">
+            <TextField hintText="Type your name" />
+            <RaisedButton className="join-button" label="Join" />
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
