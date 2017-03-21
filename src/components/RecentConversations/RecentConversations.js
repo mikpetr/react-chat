aa8ed388392cbd2680e08879c3596e6538e662c9
Michael Petrosyan <mikpetr95@gmail.com>
2017-03-21 10:27:34 +0400

    Used states instead of direct object

--- src/components/RecentConversations/RecentConversations.js
+++ src/components/RecentConversations/RecentConversations.js
@@ -7,32 +7,34 @@ import Divider from 'material-ui/Divider';
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 
 class RecentConversations extends Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
 
-    this.conversations = [
-      {
-        avatar: 'images/ok-128.jpg',
-        name: 'Brunch this weekend?',
-        lastMessage: 'Brendan Lim -- I\'ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?'
-      }, {
-        avatar: 'images/kolage-128.jpg',
-        name: 'Summer BBQ -- 4',
-        lastMessage: 'to me, Scott, Jennifer -- Wish I could come, but I\'m out of town this weekend.'
-      }, {
-        avatar: 'images/uxceo-128.jpg',
-        name: 'Oui oui',
-        lastMessage: 'Grace Ng -- Do you have Paris recommendations? Have you ever been?'
-      }, {
-        avatar: 'images/kerem-128.jpg',
-        name: 'Birdthday gift',
-        lastMessage: 'Kerem Suer -- Do you have any ideas what we can get Heidi for her birthday? How about a pony?'
-      }, {
-        avatar: 'images/raquelromanp-128.jpg',
-        name: 'Recipe to try',
-        lastMessage: 'Raquel Parrado -- We should eat this: grated squash. Corn and tomatillo tacos.'
-      }
-    ];
+    this.state = {
+      conversations: [
+        {
+          avatar: 'images/ok-128.jpg',
+          name: 'Brunch this weekend?',
+          lastMessage: 'Brendan Lim -- I\'ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?'
+        }, {
+          avatar: 'images/kolage-128.jpg',
+          name: 'Summer BBQ -- 4',
+          lastMessage: 'to me, Scott, Jennifer -- Wish I could come, but I\'m out of town this weekend.'
+        }, {
+          avatar: 'images/uxceo-128.jpg',
+          name: 'Oui oui',
+          lastMessage: 'Grace Ng -- Do you have Paris recommendations? Have you ever been?'
+        }, {
+          avatar: 'images/kerem-128.jpg',
+          name: 'Birdthday gift',
+          lastMessage: 'Kerem Suer -- Do you have any ideas what we can get Heidi for her birthday? How about a pony?'
+        }, {
+          avatar: 'images/raquelromanp-128.jpg',
+          name: 'Recipe to try',
+          lastMessage: 'Raquel Parrado -- We should eat this: grated squash. Corn and tomatillo tacos.'
+        }
+      ]
+    };
   }
 
   getConversationsList (conversations) {
@@ -60,7 +62,7 @@ class RecentConversations extends Component {
     return (
       <List>
         <Subheader>Recent conversations</Subheader>
-        {this.getConversationsList(this.conversations)}
+        {this.getConversationsList(this.state.conversations)}
       </List>
     );
   }
